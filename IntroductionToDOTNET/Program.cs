03a948b2292be489a5c14a58080d888d31e7037c
user <user@WIN-RV3D993QKOS>
2025-10-17 21:55:40 +0300

    Add project files.

---
+++ IntroductionToDOTNET/Program.cs
@@ -0,0 +1,46 @@
+﻿//#define CONSOLE
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToDOTNET
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+#if CONSOLE
+            Console.WriteLine("Hello World!");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("Hello .NET\t");
+            Console.WriteLine();
+            Console.Title = "Intorduction to .NET";
+            Console.Beep(37, 2000);
+            Console.CursorLeft = 25;
+            Console.CursorTop = 5;
+            Console.WriteLine("SetConsoleCursorPosition");
+            Console.SetCursorPosition(22, 8);
+            Console.WriteLine("Another position");
+            Console.ResetColor();
+            Console.Clear(); 
+#endif
+
+			Console.Write("Введите Ваше имя:");
+			string firstName = Console.ReadLine();
+
+			Console.Write("Введите Вашу фамилию:");
+			string lastName = Console.ReadLine();
+
+			Console.Write("Введите Ваш возраст: ");
+			int age = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine(lastName + " " + firstName + " " + age);
+			Console.WriteLine(String.Format("{0} {1} {2}", lastName, firstName, age));
+            Console.WriteLine($"{lastName} {firstName} {age}");
+
+        }
+	}
+}
